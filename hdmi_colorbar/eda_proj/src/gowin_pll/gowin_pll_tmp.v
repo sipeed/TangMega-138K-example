f609cd6de6a9d5c23507a6c92a8429f598ab7d28
AlanCui <me@alancui.cc>
2024-03-12 14:39:28 +0800

    add: test for hdmi (dvp)

---
+++ hdmi_colorbar/eda_proj/src/gowin_pll/gowin_pll_tmp.v
@@ -0,0 +1,20 @@
+//Copyright (C)2014-2024 Gowin Semiconductor Corporation.
+//All rights reserved.
+//File Title: Template file for instantiation
+//Tool Version: V1.9.9.01 (64-bit)
+//Part Number: GW5AST-LV138PG484AC2/I1
+//Device: GW5AST-138B
+//Device Version: B
+//Created Time: Tue Mar 12 14:17:48 2024
+
+//Change the instance name and port connections to the signal names
+//--------Copy here to design--------
+
+    Gowin_PLL your_instance_name(
+        .lock(lock_o), //output lock
+        .clkout0(clkout0_o), //output clkout0
+        .clkout1(clkout1_o), //output clkout1
+        .clkin(clkin_i) //input clkin
+    );
+
+//--------Copy end-------------------
